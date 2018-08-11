fa6f3a21ec1f86d9fa688805bc6d0a64e27627d0
Tigrou Ind <tigrou.ind@gmail.com>
2018-08-11 14:18:11 +0200

    Spaces to tab

--- Emulation/SimonEm/SimonEm/Main.cs
+++ Emulation/SimonEm/SimonEm/Main.cs
@@ -13,146 +13,146 @@ using NAudio.Wave;
 namespace SimonEm
 {
 
-    public partial class Main : Form
-    {
-        private SimonHardware simon = new SimonHardware();
+	public partial class Main : Form
+	{
+		private SimonHardware simon = new SimonHardware();
 
-        private static String[] registers = { "3pctr", "1a", "1x", "1y", "2xy", "1s", "1pa", "1pb", "2pc", "2sr", "1sl", "1cl","2o","4cycles" };
+		private static String[] registers = { "3pctr", "1a", "1x", "1y", "2xy", "1s", "1pa", "1pb", "2pc", "2sr", "1sl", "1cl","2o","4cycles" };
 
-        private Stopwatch stopwatch = new Stopwatch();
-        private Label[] registerLabels;
-        private int[] registerWidths;
-        private IList<String> disassembly;
-        private int[] codeItem = new int[1024];
-        private bool[] LightStatus = new bool[4];
-        private float[] LightTimers = new float[4];
-        private WaveOut waveout;
+		private Stopwatch stopwatch = new Stopwatch();
+		private Label[] registerLabels;
+		private int[] registerWidths;
+		private IList<String> disassembly;
+		private int[] codeItem = new int[1024];
+		private bool[] LightStatus = new bool[4];
+		private float[] LightTimers = new float[4];
+		private WaveOut waveout;
 
-        public Main()
-        {             
-            InitializeComponent();
-            registerLabels = new Label[registers.Length];
-            registerWidths = new int[registers.Length];
-            Font lblFont = new Font(FontFamily.GenericSansSerif, 11);
-            for (int i = 0; i < registers.Length; i++)
-            {
-                int x = cpuGroup.Left + 16;
-                int y = cpuGroup.Top + 16 + (cpuGroup.Height - 32) * i / (registers.Length-0);
-                Label lbl = new Label();
-                y = y + lbl.Height / 2;
-                lbl.Text = registers[i].Substring(1).ToUpper() + ":";
-                lbl.Left = x; lbl.Top = y;
-                this.Controls.Add(lbl);
-                lbl.BringToFront();
-                lbl.Font = lblFont;
+		public Main()
+		{			  
+			InitializeComponent();
+			registerLabels = new Label[registers.Length];
+			registerWidths = new int[registers.Length];
+			Font lblFont = new Font(FontFamily.GenericSansSerif, 11);
+			for (int i = 0; i < registers.Length; i++)
+			{
+				int x = cpuGroup.Left + 16;
+				int y = cpuGroup.Top + 16 + (cpuGroup.Height - 32) * i / (registers.Length-0);
+				Label lbl = new Label();
+				y = y + lbl.Height / 2;
+				lbl.Text = registers[i].Substring(1).ToUpper() + ":";
+				lbl.Left = x; lbl.Top = y;
+				this.Controls.Add(lbl);
+				lbl.BringToFront();
+				lbl.Font = lblFont;
 
-                registerWidths[i] = Int32.Parse(registers[i].Substring(0, 1));
-                registerLabels[i] = new Label();
-                registerLabels[i].Left = x + 72; registerLabels[i].Top = y;
-                registerLabels[i].Text = "XXXXXX".Substring(0, registerWidths[i]);
-                this.Controls.Add(registerLabels[i]);
-                registerLabels[i].BringToFront();
-                registerLabels[i].Font = lblFont;
-                registerLabels[i].Width = 48;
-            }
-            for (int i = 0; i < 16; i++)
-            {
-                ramView.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
-            }
-            disassembly = simon.getAssemblerCode();
-            foreach (String s in disassembly)
-            {
-            	if (s != "" && !s.StartsWith("Page", StringComparison.InvariantCulture))
-                {
-                    int address = Convert.ToInt32(s.Substring(0, 3), 16);
-                    codeItem[address] = codeListBox.Items.Count;
-                }
-                codeListBox.Items.Add(s);
-            }
-            RefreshProcessor();
-                                    
-            //setup sound
-            waveout = new WaveOut();
-		    SampleProvider sampleProvider = new SampleProvider(simon);		  	   
-		    waveout.DesiredLatency = 100;
-		    waveout.Init(sampleProvider);		   
-        }
-        
-        public void Stop()
-        {
-        	if(timer1.Enabled)
-        	{
-	        	Console.WriteLine("stop");
-	        	timer1.Enabled = false;
-	        	RefreshProcessor();
-        	}
-        }
-
-
-        private void RefreshProcessor()
-        {
-            String[] data;
-            IDictionary<string, int> status = simon.getStatus();
-            for (int i = 0; i < registers.Length; i++)
-            {
-                String text = status[registers[i].Substring(1)].ToString("X"+(registerWidths[i].ToString()));
-                registerLabels[i].Text = text;
-            }
-            ramView.Rows.Clear();
-            for (int row = 0; row < 4; row++)
-            {
-                data = new String[16];
-                for (int col = 0; col < 16; col++)
-                    data[col] = "    " + simon.getRAMMemory(row * 16 + col).ToString("X1") + "    ";
-                ramView.Rows.Add(data);
-            }
-            ramView.Rows.Add("");
-            data = new String[16];
-            for (int col = 0; col <= 10; col++)
-                data[col] = status["r" + col.ToString()].ToString();
-            ramView.Rows.Add(data);
-            ramView.CurrentCell = ramView.Rows[status["x"]].Cells[status["y"]];
-
-            int pctr = codeItem[status["pctr"]];
-            codeListBox.SelectionMode = SelectionMode.MultiExtended ;
-            codeListBox.ClearSelected();
-            codeListBox.SelectedIndex = pctr;
-            codeListBox.TopIndex = pctr - 4;
-            
-        }
-
-        private void singleStepToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        	timer1.Enabled = false;
-        	runToolStripMenuItem.Enabled = true;
-        	stopToolStripMenuItem.Enabled = true;
-            simon.execute();
-            RefreshProcessor();            
-        }
-        
-        void RunToolStripMenuItem1Click(object sender, EventArgs e)
+				registerWidths[i] = Int32.Parse(registers[i].Substring(0, 1));
+				registerLabels[i] = new Label();
+				registerLabels[i].Left = x + 72; registerLabels[i].Top = y;
+				registerLabels[i].Text = "XXXXXX".Substring(0, registerWidths[i]);
+				this.Controls.Add(registerLabels[i]);
+				registerLabels[i].BringToFront();
+				registerLabels[i].Font = lblFont;
+				registerLabels[i].Width = 48;
+			}
+			for (int i = 0; i < 16; i++)
+			{
+				ramView.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+			}
+			disassembly = simon.getAssemblerCode();
+			foreach (String s in disassembly)
+			{
+				if (s != "" && !s.StartsWith("Page", StringComparison.InvariantCulture))
+				{
+					int address = Convert.ToInt32(s.Substring(0, 3), 16);
+					codeItem[address] = codeListBox.Items.Count;
+				}
+				codeListBox.Items.Add(s);
+			}
+			RefreshProcessor();
+									
+			//setup sound
+			waveout = new WaveOut();
+			SampleProvider sampleProvider = new SampleProvider(simon);			   
+			waveout.DesiredLatency = 100;
+			waveout.Init(sampleProvider);		   
+		}
+		
+		public void Stop()
 		{
-        	cyclesSoFar = 0;
-        	stopwatch.Restart();
-        	waveout.Play();
-        	
-        	runToolStripMenuItem.Enabled = false;
-        	stopToolStripMenuItem.Enabled = true;
+			if(timer1.Enabled)
+			{
+				Console.WriteLine("stop");
+				timer1.Enabled = false;
+				RefreshProcessor();
+			}
+		}
+
+
+		private void RefreshProcessor()
+		{
+			String[] data;
+			IDictionary<string, int> status = simon.getStatus();
+			for (int i = 0; i < registers.Length; i++)
+			{
+				String text = status[registers[i].Substring(1)].ToString("X"+(registerWidths[i].ToString()));
+				registerLabels[i].Text = text;
+			}
+			ramView.Rows.Clear();
+			for (int row = 0; row < 4; row++)
+			{
+				data = new String[16];
+				for (int col = 0; col < 16; col++)
+					data[col] = "	 " + simon.getRAMMemory(row * 16 + col).ToString("X1") + "	  ";
+				ramView.Rows.Add(data);
+			}
+			ramView.Rows.Add("");
+			data = new String[16];
+			for (int col = 0; col <= 10; col++)
+				data[col] = status["r" + col.ToString()].ToString();
+			ramView.Rows.Add(data);
+			ramView.CurrentCell = ramView.Rows[status["x"]].Cells[status["y"]];
+
+			int pctr = codeItem[status["pctr"]];
+			codeListBox.SelectionMode = SelectionMode.MultiExtended ;
+			codeListBox.ClearSelected();
+			codeListBox.SelectedIndex = pctr;
+			codeListBox.TopIndex = pctr - 4;
+			
+		}
+
+		private void singleStepToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			timer1.Enabled = false;
+			runToolStripMenuItem.Enabled = true;
+			stopToolStripMenuItem.Enabled = true;
+			simon.execute();
+			RefreshProcessor();			   
+		}
+		
+		void RunToolStripMenuItem1Click(object sender, EventArgs e)
+		{
+			cyclesSoFar = 0;
+			stopwatch.Restart();
+			waveout.Play();
+			
+			runToolStripMenuItem.Enabled = false;
+			stopToolStripMenuItem.Enabled = true;
 			timer1.Enabled = true;
 		}
-        
-        void StopToolStripMenuItem1Click(object sender, EventArgs e)
+		
+		void StopToolStripMenuItem1Click(object sender, EventArgs e)
 		{
-        	simon.reset();
-        	waveout.Stop();
-        	
-        	timer1.Enabled = false;
-        	stopToolStripMenuItem.Enabled = false;
-        	runToolStripMenuItem.Enabled = true;
-        	RefreshProcessor();          
+			simon.reset();
+			waveout.Stop();
+			
+			timer1.Enabled = false;
+			stopToolStripMenuItem.Enabled = false;
+			runToolStripMenuItem.Enabled = true;
+			RefreshProcessor();			 
 		}
-        
-        int cyclesSoFar;        
+		
+		int cyclesSoFar;		
 		void Timer1Tick(object sender, EventArgs e)
 		{
 			//300000hz / 6 cycles = 50000hz
@@ -259,7 +259,7 @@ namespace SimonEm
 			Rectangle rect = new Rectangle(10, 10, 280, 280);
 			Rectangle outerRectangle = new Rectangle(new Point(rect.Left, rect.Top), rect.Size);
 			Rectangle innerRectangle = new Rectangle(new Point(rect.Left+rect.Size.Width / 4, rect.Top+rect.Size.Height / 4),
-			                                         new Size(rect.Size.Width / 2, rect.Size.Height / 2));
+													 new Size(rect.Size.Width / 2, rect.Size.Height / 2));
 			for (int i = 0; i < 4; i++)
 			{ 
 				if(e.ClipRectangle.IntersectsWith(clipZones[i]))
@@ -333,5 +333,5 @@ namespace SimonEm
 			}
 		}
 		
-    }
+	}
 }
