9662d9d9ab2e4def4036a6d816938b9eebf105ff
Tigrou Ind <tigrou.ind@gmail.com>
2023-06-30 23:11:38 +0200

    Remove unused references + migrate to PackageReference

--- Emulation/SimonEm/SimonEm/Main.cs
+++ Emulation/SimonEm/SimonEm/Main.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using NAudio.Wave;
 
